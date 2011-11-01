d8194043790bd78cda430d7b213cd534349f4d52
Luka Pušić <pusic93@gmail.com>
2011-11-01 18:19:56 +0100

    first commit

---
+++ soundcloud.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#soundcloud music downloader by http://360percents.com - v3.0 on Nov 1st 2011
+#Author: Luka Pusic <pusic93@gmail.com>
+
+echo "[i] soundcloud.com music downloader by http://360percents.com (wget version)";
+
+if [ -z "$1" ]; then
+	echo "";echo "[i] Usage: `basename $0` [DJ-URL]";echo "";exit
+fi
+
+pages=`wget "$1/tracks" -q --user-agent 'Mozilla/5.0' -O - | tr '"' "\n" | grep "tracks?page=" | sort -u | tail -n 1 | cut -d "=" -f 2`
+
+if [ -z "$pages" ]; then
+	pages=1
+fi
+
+echo "[i] Found $pages pages of songs!"
+for (( page=1; page <= $pages; page++ ))
+do
+if [ "$pages" = "1" ]; then
+	this=`wget -q --user-agent='Mozilla/5.0' $1 -O -`;
+else
+	this=`wget -q --user-agent='Mozilla/5.0' $1/tracks?page=$page -O -`;
+fi
+songs=`echo "$this" | grep 'streamUrl' | tr '"' "\n" | grep 'http://media.soundcloud.com/stream/'`;
+songcount=`echo "$songs" | wc -l`
+titles=`echo "$this" | grep 'title":"' | tr ',' "\n" | grep 'title' | cut -d '"' -f 4`
+
+if [ -z "$songs" ]; then
+	echo "[!] No song found at $1/tracks?page=$page." && exit
+fi
+
+echo "[+] Downloading $songcount songs from page $page..."
+
+for (( songid=1; songid <= $songcount; songid++ ))
+do
+	title=`echo "$titles" | sed -n "$songid"p`
+	echo "[-] Downloading $title..."
+	url=`echo "$songs" | sed -n "$songid"p`
+	wget -q --user-agent='Mozilla/5.0' -O "$title.mp3" $url;
+done
+done
